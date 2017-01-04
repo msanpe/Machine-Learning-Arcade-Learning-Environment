fe3c751d9154bfd25bd253cfd4169575d288187d
Cristina Rivera <cristina.rivera.baydal@gmail.com>
2017-01-04 13:19:41 +0100

    Linear Regression (final)

--- LinearRegression/linearregression.cpp
+++ LinearRegression/linearregression.cpp
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /* 
- * File:   main.cpp
  * Author: cristina
- *
- * Created on January 3, 2017, 10:14 PM
  */
 
 #include <cstdlib>
