847f9843eff33010bf9ebb8bdc45ca68f2eb4b03
Cristina Rivera <cristina.rivera.baydal@gmail.com>
2017-01-07 13:28:54 +0100

    Modificación para n características

--- LinearRegression/linearregression.cpp
+++ LinearRegression/linearregression.cpp
@@ -31,6 +31,7 @@ class LinearRegression{
     float getErr(int idx);
     void actualizarB(float alpha, float err, int idx);
     void train(float alpha);
+    int getTam();
 };
 int LinearRegression::getN(){
     return N;
@@ -61,25 +62,31 @@ void LinearRegression::init(string file){
     //outputs.resize(tam); 
     b.resize(tam); 
     
-    float x0, x1, y;
     int n = 0;
-    while(ifs >> x0 >> x1 >> y){
-        inputs[0].push_back(x0);
-        inputs[1].push_back(x1);
-        outputs.push_back(y);
+    string s;
+    while(ifs >> s){
+        for(int i=0; i<tam-1; i++){
+            inputs[i].push_back(atof(s.c_str()));
+            ifs >> s;
+        }
+        outputs.push_back(atof(s.c_str()));
         n++;
     }
     N = n;   
 }
 void LinearRegression::actualizarB(float alpha, float err, int idx){    
     b[0] = b[0] - alpha * err;
-    b[1] = b[1] - alpha * err * inputs[0][idx];
-    b[2] = b[2] - alpha * err * inputs[1][idx];
+    for(int i=1; i<tam; i++){
+        b[i] = b[i] - alpha * err * inputs[i-1][idx];
+    }
 }
 float LinearRegression::getB(int n){
     return b[n];
 }
 
+int LinearRegression::getTam(){
+    return tam;
+}
 void LinearRegression::train(float alpha){
     float p;
     float err;
@@ -89,8 +96,9 @@ void LinearRegression::train(float alpha){
         this->actualizarB(alpha, err, i);
     }
     cout << "b0 = " << this->getB(0) ;
-    cout << ", b1 = " << this->getB(1) ;
-    cout << ", b2 = " << this->getB(2) ;
+    for (int j = 1; j <getTam(); j++){
+       cout << ", b" << j << " = " <<  this->getB(j) ;
+    }
     cout << ", error " << cont++ << " " << err << endl;
 }
 int main(int argc, char** argv) {
@@ -100,11 +108,18 @@ int main(int argc, char** argv) {
     lin_reg.init(file);
     
     float alpha = 0.001;
-
-    for (int i = 0; i < 20; i ++) {
+    int epoches = 20;
+    for (int i = 0; i < epoches; i ++) {
         lin_reg.train(alpha);
     }
-    cout << "\nFunción regresión lineal:    y = " << lin_reg.getB(0) << " + " << lin_reg.getB(1) << " * x1 + " << lin_reg.getB(2) << " * x2" << endl;
+    
+    cout << "\nFunción regresión lineal:    y = (" ;
+    cout << lin_reg.getB(0) << ")" ;
+    for(int j = 1; j<lin_reg.getTam(); j++){
+        cout << " + (" << lin_reg.getB(j) << ") * x" << j;
+    }
+    cout << endl;
+    
     return 0;
 }
 
